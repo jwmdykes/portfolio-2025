59e706b8b78af19f5224464747606eb853b73650
John Dykes <98johndykes@gmail.com>
2025-04-17 15:43:02 -0400

    add meta tags and title, robots.txt

--- src/pages/+config.ts
+++ src/pages/+config.ts
@@ -1,6 +1,10 @@
 import vikeReact from 'vike-react/config';
+import { Config } from 'vike/types';
 
 // https://vike.dev/config
 export default {
+  title: 'John Dykes',
+  description:
+    'John Dykes is a full stack web and AI developer with years of experience creating performant and visually stunning React JS applications.',
   extends: [vikeReact],
-};
+} satisfies Config;
